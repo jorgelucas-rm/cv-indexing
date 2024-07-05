1cbfe9d2f169223c745d7bd9ddc731d5a95dea11
Jorge Lucas Rodrigues Martins <jorgelucas.rm@outlook.com>
2024-07-05 13:45:04 +0000

    Merged PR 2995: (13840)[Backend]Fix: improve the level of reliability of the return
    We improve the reliability level with a new AI trained model, and to use him, we add a mapping called classMapping. To guarantee a quality return, we define a limit of 30% position probability, positions with less than 30% will not be include on him. On this version we also update the requirements and delete the CleaningService.
    
    Related work items: #13840

---
+++ API/model/mapping.py
@@ -0,0 +1,36 @@
+class mapping:
+
+    classMapping = {
+        "ACCOUNTANT": 0,
+        "ADVOCATE": 1,
+        "AGRICULTURE": 2,
+        "APPAREL": 3,
+        "ARTS": 4,
+        "AUTOMOBILE": 5,
+        "AVIATION": 6,
+        "BANKING": 7,
+        "BPO": 8,
+        "BUSINESS-DEVELOPMENT": 9,
+        "CHEF": 10,
+        "CONSTRUCTION": 11,
+        "CONSULTANT": 12,
+        "DESIGNER": 13,
+        "DIGITAL-MEDIA": 14,
+        "ENGINEERING": 15,
+        "FINANCE": 16,
+        "FITNESS": 17,
+        "HEALTHCARE": 18,
+        "HR": 19,
+        "INFORMATION-TECHNOLOGY": 20,
+        "PUBLIC-RELATIONS": 21,
+        "SALES": 22,
+        "TEACHER": 23,
+        "ARCHITECTS": 24,
+        "MANAGMENT": 25,
+    }
+
+    levelMapping = {
+        "Junior": 0,
+        "Mid-level": 1,
+        "Senior": 2,
+    }
--- API/service/PDFAnalysisService.py
+++ API/service/PDFAnalysisService.py
@@ -1,14 +1,18 @@
 import os
 
 import joblib
+import numpy as np
+import spacy
 from dotenv import load_dotenv
+from model.mapping import mapping
 from service.ExeptionService import CustomException, PDFErrorType
-from service.TextCleanigService import TextCleaningService as tc
+from spacypdfreader.spacypdfreader import pdf_reader
 
 load_dotenv()
+nlp = spacy.load(os.getenv("spacy_en"))
 directory = os.getenv("file_path")
-model = joblib.load("model/cv_ai")
-vectorizer = joblib.load("model/cv_vectorizer")
+model = joblib.load(os.getenv("model"))
+vectorizer = joblib.load(os.getenv("vectorizer"))
 
 
 class PDFAnalysisService:
@@ -19,20 +23,25 @@ class PDFAnalysisService:
         if not os.path.exists(pdf):
             return CustomException(PDFErrorType.FILE_NOT_FOUND)
 
-        resume = tc.cleanText(pdf)
+        resume = pdf_reader(pdf, nlp)
 
-        prev = vectorizer.transform([resume])
+        prev = vectorizer.transform([resume.text])
 
         probabilities = model.predict_proba(prev)
 
-        roleProbabilities = {
-            role: prob for role, prob in zip(model.classes_, probabilities[0])
-        }
+        top_indices = np.argsort(probabilities[0])[::-1][:3]
 
-        topProbabilities = dict(
-            sorted(roleProbabilities.items(), key=lambda item: item[1], reverse=True)[
-                :3
-            ]
-        )
+        topProbabilities = []
+
+        for idx in top_indices:
+            class_name = [
+                key for key, value in mapping.classMapping.items() if value == idx
+            ][0]
+            if probabilities[0][idx] * 100 > 30.0:
+                prob_percent = probabilities[0][idx] * 100
+                topProbabilities.append(f"{class_name}: {prob_percent:.2f}%")
+
+        if topProbabilities == []:
+            topProbabilities = ["undefined"]
 
         return {"The resume fits into the following professions:": topProbabilities}
